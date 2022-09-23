4bd9d74abaf85a60e95d03f9b23b623c49fe3ec1
Fazekas Milán <fazekas.milan1@gmail.com>
2022-09-23 20:21:51 +0200

    second and third days

--- src/routes/nap/2/+page.svelte
+++ src/routes/nap/2/+page.svelte
@@ -3,5 +3,20 @@
 </script>
 
 <Content>
+    <h1>This is the second day!</h1>
+    <h4>2022.09.08.</h4>
+    <h2>Fertőd</h2>
+    <h3>Eszterházy-kastély</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
     
+    <h2>Fertőrákos</h2>
+    <h3>Kőfejtő</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
+
+    <h2>Sopron</h2>
+    <h3>Tűztorony</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
 </Content>
--- src/routes/nap/3/+page.svelte
+++ src/routes/nap/3/+page.svelte
@@ -2,5 +2,20 @@
     import Content from "../../../lib/components/Content.svelte";
 </script>
 <Content>
+    <h1>This is the third day!</h1>
+    <h4>2022.09.09.</h4>
+    <h2>Kőszeg</h2>
+    <h3>Jurisics-vár</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
     
+    <h2>Szombathely</h2>
+    <h3>Iseum</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
+
+    <h2>Győr</h2>
+    <h3>Püspökvár-Toronykilátó</h3>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore delectus labore tenetur eum voluptatum veniam sed enim nam officiis. Earum unde laborum corporis cumque commodi? Fugit a ipsa tempore quaerat.</p>
+    <img src="/images/radio1.jpg" alt="">
 </Content>
